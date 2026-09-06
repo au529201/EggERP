fa01529267299acf7a6d89286168efec56b799e6
au529201 <a.umpar.529201@umindanao.edu.ph>
2026-09-07 00:11:35 +0800

    Add product API endpoint

---
+++ EggERP.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using EggERP.Application.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EggERP.Web.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet("{businessId:guid}")]
+    public async Task<IActionResult> GetProducts(Guid businessId)
+    {
+        var products = await _productService.GetProductsAsync(businessId);
+
+        return Ok(products);
+    }
+}
