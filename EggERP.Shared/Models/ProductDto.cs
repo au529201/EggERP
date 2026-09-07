5ca87b2f6ffbe41d8a537eb026848ba5284df334
au529201 <a.umpar.529201@umindanao.edu.ph>
2026-09-07 13:43:52 +0800

    Add CategoryId to ProductDto for Category Filter

--- EggERP.Shared/Models/ProductDto.cs
+++ EggERP.Shared/Models/ProductDto.cs
@@ -3,6 +3,7 @@
     public class ProductDto
     {
         public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Sku { get; set; } = string.Empty;
         public string Unit { get; set; } = string.Empty;
