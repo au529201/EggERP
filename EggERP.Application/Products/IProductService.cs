e3a99461c536916b2d0c4324850ff5ba77bfd0ab
au529201 <a.umpar.529201@umindanao.edu.ph>
2026-09-07 08:56:41 +0800

    Add GetProductByIdAsync to product service

--- EggERP.Application/Products/IProductService.cs
+++ EggERP.Application/Products/IProductService.cs
@@ -5,6 +5,6 @@ namespace EggERP.Application.Products;
 public interface IProductService
 {
     Task<List<Product>> GetProductsAsync(Guid businessId);
-
+    Task<Product?> GetProductByIdAsync(Guid businessId, Guid id);
     Task<Product> CreateProductAsync(Product product);
 }
--- EggERP.Application/Products/ProductService.cs
+++ EggERP.Application/Products/ProductService.cs
@@ -1,19 +1,20 @@
 ﻿using EggERP.Domain.Entities;
-
 namespace EggERP.Application.Products;
-
 public class ProductService : IProductService
 {
     private readonly IProductRepository _productRepository;
-
     public ProductService(IProductRepository productRepository)
     {
         _productRepository = productRepository;
     }
-
     public Task<List<Product>> GetProductsAsync(Guid businessId)
     {
         return _productRepository.GetActiveByBusinessIdAsync(businessId);
+    }
+
+    public Task<Product?> GetProductByIdAsync(Guid businessId, Guid id)
+    {
+        return _productRepository.GetByIdAsync(businessId, id);
     }
 
     public async Task<Product> CreateProductAsync(Product product)
@@ -21,7 +22,6 @@ public class ProductService : IProductService
         product.Id = Guid.NewGuid();
         product.CreatedAtUtc = DateTime.UtcNow;
         product.IsActive = true;
-
         return await _productRepository.AddAsync(product);
     }
 }
