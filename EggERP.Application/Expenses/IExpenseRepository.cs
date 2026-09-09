dd9add0ef72985d326556b8be9c74b51181315f5
au529201 <a.umpar.529201@umindanao.edu.ph>
2026-09-09 19:53:30 +0800

    Add Expense repository

---
+++ EggERP.Application/Expenses/IExpenseRepository.cs
@@ -0,0 +1,8 @@
+﻿using EggERP.Domain.Entities;
+namespace EggERP.Application.Expenses;
+public interface IExpenseRepository
+{
+    Task<List<Expense>> GetByBusinessIdAsync(Guid businessId);
+    Task<Expense?> GetByIdAsync(Guid businessId, Guid id);
+    Task<Expense> AddAsync(Expense expense);
+}
---
+++ EggERP.Infrastructure/Expenses/ExpenseRepository.cs
@@ -0,0 +1,31 @@
+﻿using EggERP.Application.Expenses;
+using EggERP.Domain.Entities;
+using EggERP.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+namespace EggERP.Infrastructure.Expenses;
+public class ExpenseRepository : IExpenseRepository
+{
+    private readonly EggERPDbContext _dbContext;
+    public ExpenseRepository(EggERPDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public Task<List<Expense>> GetByBusinessIdAsync(Guid businessId)
+    {
+        return _dbContext.Expenses
+            .Where(e => e.BusinessId == businessId)
+            .OrderByDescending(e => e.ExpenseDateUtc)
+            .ToListAsync();
+    }
+    public Task<Expense?> GetByIdAsync(Guid businessId, Guid id)
+    {
+        return _dbContext.Expenses
+            .FirstOrDefaultAsync(e => e.BusinessId == businessId && e.Id == id);
+    }
+    public async Task<Expense> AddAsync(Expense expense)
+    {
+        _dbContext.Expenses.Add(expense);
+        await _dbContext.SaveChangesAsync();
+        return expense;
+    }
+}
